f0d0aa073a145cdfc364319f700d863be7ecd846
Kai-clrb <kail266rb@gmail.com>
2025-02-10 21:07:01 +0530

    Separated frontend for Fleek

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="192.168.14.57")
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
